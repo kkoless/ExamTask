a971a5dc35d5ccd82184df71af239ffc1c127e30
Kirill Kolesnikov <65421720+kkoless@users.noreply.github.com>
2023-04-24 22:52:22 +0400

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,4 +1,4 @@
-#include "example3.hpp"
+#include "task.hpp"
 
 // MARK: Удалить все строки короче 3 символов из массива строк
 // MARK: Remove all strings shorter than 3 characters from the string array
